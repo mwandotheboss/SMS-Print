a02ae0a8856413032120189deaea44b633b07596
Zephania Mwando <zephaniamwando@gmail.com>
2021-06-08 10:22:41 +0300

    Broadcast receiver created

--- app/src/main/java/com/smartwareafrica/smsprinter/MainActivity.kt
+++ app/src/main/java/com/smartwareafrica/smsprinter/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartwareafrica.smsprinter
 
+import android.content.BroadcastReceiver
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
